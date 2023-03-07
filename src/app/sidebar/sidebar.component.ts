83afb5c2f773836cabc76bb5f4982840a6a69439
DESKTOP-AFFH5NC\Robi <lobika2001@gmail.com>
2023-03-07 18:20:41 +0100

    Added mobile friendly header

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+  constructor() { }
 
+  toggleSidebar() {
+    alert('toggle sidebar');
+  }
 }
