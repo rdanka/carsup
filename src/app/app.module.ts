c67f82952ae31f7bf37b0ec31baacaaf83ecf8dc
DESKTOP-AFFH5NC\Robi <lobika2001@gmail.com>
2023-03-08 17:26:12 +0100

    Added data-toggle components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { MainDashComponent } from './main-dash/main-dash.component';
 import { OverallStatsComponent } from './main-dash/overall-stats/overall-stats.component';
 import { StatisticComponent } from './main-dash/overall-stats/statistic/statistic.component';
+import { DataPickerComponent } from './main-dash/data-picker/data-picker.component';
+import { BarChartComponent } from './main-dash/bar-chart/bar-chart.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { StatisticComponent } from './main-dash/overall-stats/statistic/statisti
     SidebarComponent,
     MainDashComponent,
     OverallStatsComponent,
-    StatisticComponent
+    StatisticComponent,
+    DataPickerComponent,
+    BarChartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-dash/data-picker/data-picker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'data-picker',
+  templateUrl: './data-picker.component.html',
+  styleUrls: ['./data-picker.component.scss']
+})
+export class DataPickerComponent {
+
+}
